57b7f896d648824ecb23ca9ed1d76c05c25f1ed7
Hxuhao233 <you@example.com>
2016-05-11 22:28:31 +0800

    hxh

--- wx_sample.php
+++ wx_sample.php
@@ -1,23 +1,17 @@
 <?php
 /**
   * wechat php test
-  * 
   */
-//echo 'start';
+
 //define your token
 define("TOKEN", "weixin");
-include_once("filterKeyword.php");
 $wechatObj = new wechatCallbackapiTest();
-//验证函数
-//$wechatObj->valid();
-//自动回复
+// $wechatObj->valid();
 $wechatObj->responseMsg();
-//echo 'hi,sb~';
 class wechatCallbackapiTest
 {
     public function valid()
     {
-        //接收随机字符串
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
@@ -27,10 +21,6 @@ class wechatCallbackapiTest
         }
     }
 
-    /**
-     * 自动回复
-     * @return [type] [description]
-     */
     public function responseMsg()
     {
         //get post data, May be due to the different environments
@@ -42,15 +32,10 @@ class wechatCallbackapiTest
                    the best way is to check the validity of xml by yourself */
                 libxml_disable_entity_loader(true);
                 $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;             //发送方账号
-                $toUsername = $postObj->ToUserName;                     //开发者微信号
-                $keyword = trim($postObj->Content);                             //文本消息
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
                 $time = time();
-                
-                //消息类型(6种)
-                $msgType = $postObj->MsgType;
-
-                //文本发送模板
                 $textTpl = "<xml>
                             <ToUserName><![CDATA[%s]]></ToUserName>
                             <FromUserName><![CDATA[%s]]></FromUserName>
@@ -58,79 +43,46 @@ class wechatCallbackapiTest
                             <MsgType><![CDATA[%s]]></MsgType>
                             <Content><![CDATA[%s]]></Content>
                             <FuncFlag>0</FuncFlag>
-                            </xml>";        
-
-                switch ($msgType) {
-                    case 'text':
-                        # code...
-                        //回复类型
-                        $msgType = "text";
-             
-                        //回复内容
-                     //   $contentStr = keywordSelect($keyword);
-                       // if($contentStr=="")
-                            $contentStr ="ss";
-                        //格式化字符串
-                        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                        echo $resultStr;
-                         
-                        break;
-                    
-                    default:
-                        # code...
-                        //回复类型
-                        $msgType = "text";
-        
-                      
-                        $returnstr = "excuse me?";
-                        //格式化字符串
-                        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $returnstr);
-                        echo $resultStr;
-                        break;
+                            </xml>";             
+                if(!empty( $keyword ))
+                {
+                    $msgType = "text";
+                    $contentStr = "Welcome to wechat world!";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
+                }else{
+                    echo "Input something...";
                 }
 
-
-
-
         }else {
-                    $msgType = "text";
-         
-                    $returnstr = "excuse me?!";
-                    //格式化字符串
-                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $returnstr);
-                    echo $resultStr;
-                    exit;
+            echo "";
+            exit;
         }
     }
         
-    /**
-     * 用户签名认证
-     * @return [type] [description]
-     */
     private function checkSignature()
     {
-    // you must define TOKEN by yourself
-    if (!defined("TOKEN")) {
-        throw new Exception('TOKEN is not defined!');
-    }
-    
-    $signature = $_GET["signature"];
-    $timestamp = $_GET["timestamp"];
-    $nonce = $_GET["nonce"];
-            
-    $token = TOKEN;
-    $tmpArr = array($token, $timestamp, $nonce);
-    // use SORT_STRING rule
-    sort($tmpArr, SORT_STRING);
-    //数组转字符串再加密
-    $tmpStr = implode( $tmpArr );
-    $tmpStr = sha1( $tmpStr );
-    
-    if( $tmpStr == $signature ){
-        return true;
-    }else{
-        return false;
-    }
+        // you must define TOKEN by yourself
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
+        
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+                
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
