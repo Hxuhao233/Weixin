b1b271e1c8d132d829aff999a6f578a35dc8c20b
Hxuhao233 <you@example.com>
2016-05-11 22:59:20 +0800

    hxh

--- wx_sample.php
+++ wx_sample.php
@@ -47,18 +47,18 @@ class wechatCallbackapiTest
                 if(!empty( $keyword ))
                 {
                     $msgType = "text";
-                    $contentStr ="sb ";
-                    switch ($keyword) {
-                        case '1':
-                            # code...
-                           $contentStr = "sb wake up!!" ."233";
-                            break;
+                    $contentStr =keywordSelect($keyword);
+                    // switch ($keyword) {
+                    //     case '1':
+                    //         # code...
+                    //        $contentStr = "sb wake up!!" ."233";
+                    //         break;
                         
-                        default:
-                            # code...
-                            $contentStr =  "balabala"; 
-                            break;
-                    }
+                    //     default:
+                    //         # code...
+                    //         $contentStr =  "balabala"; 
+                    //         break;
+                    // }
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                     echo $resultStr;
                 }else{
@@ -98,73 +98,5 @@ class wechatCallbackapiTest
 }
 
 
-function keywordSelect($keyword){
-    $returnstr ="heihei";
-    /*
-    switch ($keyword) {
-        case '?':
-        case "？":
-            # code...
-            //setcookie("choose","",time()-3600);
-            $returnstr = "你可能想知道\n";
-            //$returnstr += ."【1】号码\n【2】我名字= -\n";
-            
-            break;
-
-        case "1":
-            if(!isset($_COOKIE["choose"])){
-
-                $returnstr += ."你可能想知道\n";
-                $returnstr += ."【1】特殊服务号码\n【2】通讯服务号码\n【3】本人手机号码\n";
-            //  setcookie("choose","num",time()+3600);
-            
-            }else{
-                switch($_COOKIE["choose"]){
-                    case "num":
-
-                    $returnstr += "报警 110\n";
-                    break;
-                }
-            }
-            
-            break;
-        case "2":
-            if(!isset($_COOKIE["choose"])){
-
-                $returnstr += ."我名字就在写在上面啊sx\n";
-
-                
-            
-            }else{
-                switch($_COOKIE["choose"]){
-                    case "num":
-                        $returnstr += ."移动 10086\n";
-                        break;
-                }
-            
-            }
-
-            break;
-            
-        case "3":
-            if(!isset($_COOKIE["choose"])){
-                $returnstr +=. "13710685836 别打骚扰电话哦XD\n";           
-            }else{
-                switch($_COOKIE["choose"]){
-                    case "num":
-                        $returnstr +=. "移动 10086\n";
-                        break;
-                }
-            }
-
-            break;
-
-        default:
-            # code...
-            $returnstr  ="excuse me?!";
-            break;
-    }*/
-    return $keyword;
-}
 
 ?>
